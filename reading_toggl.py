694b4a9beff774b34abe09a9a124cec76dc110f3
Álvaro García Sánchez <alvaroelectronico@gmail.com>
2021-09-03 19:11:11 +0200

    All entries read within two dates Entries stored in json files, one per day.

--- reading_toggl.py
+++ reading_toggl.py
@@ -1,5 +1,7 @@
 from toggl.TogglPy import Toggl
 import pandas as pd
+import json
+from datetime import *
 
 # create a Toggl object and set our API key
 toggl = Toggl()
@@ -11,23 +13,48 @@ f.close()
 toggl.setAPIKey(toggl_token)
 
 # Request parameters. So far, only start date
-request_config = dict(
-    since="2021-01-01"
-)
 
-# This returns information for all entries
-time_entries = toggl.request("https://api.track.toggl.com/api/v8/time_entries", parameters=request_config)
+def get_json_entries(toggl, start_date="2021-09-03", end_date="2021-09-03"):
+    request_config = dict(
+        start_date="{}T00:00:00+02:00".format(start_date),
+        end_date="{}T23:59:59+02:00".format(end_date)
+    )
 
-# This list contains al entry ids to get the detailed info of each of them later on
-time_entry_ids = [i['id'] for i in time_entries]
+    # This returns information for all entries
+    time_entries = toggl.request("https://api.track.toggl.com/api/v8/time_entries", parameters=request_config)
 
-# json format dict with all data from entries
-all_entries = [toggl.request("https://api.track.toggl.com/api/v8/time_entries/{}".format(i))['data']
-               for i in time_entry_ids]
+    # This list contains al entry ids to get the detailed info of each of them later on
+    time_entry_ids = [i['id'] for i in time_entries]
 
-# json to dataframe
-df_toggl = pd.DataFrame.from_dict(all_entries)
+    # json format dict with all data from entries
+    all_entries = [toggl.request("https://api.track.toggl.com/api/v8/time_entries/{}".format(i))['data']
+                   for i in time_entry_ids]
 
-# Checking if since and until worked fine
-print("Records from: {} to {}".format(df_toggl.start.min(), df_toggl.start.max()))
-print(df_toggl.start.tail(1))
+    return all_entries
+
+def entries_to_json_file(entries, file_name):
+    json_output = json.dumps(entries)
+    with open('{}.json'.format(file_name), 'w') as f:
+        f.write(json_output)
+
+
+start_date = "2021-09-01"
+end_date = "2021-09-03"
+path = "..\data"
+
+entries = get_json_entries(toggl, start_date=start_date, end_date=end_date)
+df = pd.DataFrame.from_dict(entries)
+df["date"] = df["start"].apply(
+            lambda x: datetime.fromisoformat(x).date()
+        )
+dates = pd.date_range(start_date, end_date).tolist()
+dates = [datetime.date(d) for d in dates]
+
+for d in dates:
+    df_to_json = df[df.date == d]
+    entries = df_to_json.to_json
+    file_path = "{}/{}".format(path, d)
+    entries_to_json_file(entries, file_path)
+
+
+
