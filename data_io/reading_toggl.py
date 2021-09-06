d2f59acc5d384f1b2f309b5efad59fbe0562a50b
Álvaro García Sánchez <alvaroelectronico@gmail.com>
2021-09-06 13:39:18 +0200

    workspace read and merged into df_toggl asignacion.xlsx read basic preliminary reporting

--- data_io/reading_toggl.py
+++ data_io/reading_toggl.py
@@ -7,26 +7,44 @@ from datetime import *
 
 # Request parameters. So far, only start date
 
+
 def get_toggl(toggl, start_date="2021-09-03", end_date="2021-09-03"):
     request_config = dict(
         start_date="{}T00:00:00+02:00".format(start_date),
-        end_date="{}T23:59:59+02:00".format(end_date)
+        end_date="{}T23:59:59+02:00".format(end_date),
     )
 
     # This returns information for all entries
-    time_entries = toggl.request("https://api.track.toggl.com/api/v8/time_entries", parameters=request_config)
+    time_entries = toggl.request(
+        "https://api.track.toggl.com/api/v8/time_entries", parameters=request_config
+    )
 
     # This list contains al entry ids to get the detailed info of each of them later on
-    time_entry_ids = [i['id'] for i in time_entries]
+    time_entry_ids = [i["id"] for i in time_entries]
 
     if len(time_entries) == 0:
-        df_toggl = pd.DataFrame(columns=['date', 'client', 'project', 'h_registradas', 'description', 'start', 'lunes_semana'])
+        df_toggl = pd.DataFrame(
+            columns=[
+                "date",
+                "client",
+                "project",
+                "h_registradas",
+                "description",
+                "start",
+                "lunes_semana",
+                "workspace"
+            ]
+        )
         df_toggl
         return df_toggl
 
     entries = list()
     for i in time_entry_ids:
-        entries.append(toggl.request("https://api.track.toggl.com/api/v8/time_entries/{}".format(i))['data'])
+        entries.append(
+            toggl.request(
+                "https://api.track.toggl.com/api/v8/time_entries/{}".format(i)
+            )["data"]
+        )
         print(time_entry_ids.index(i))
         sleep(0.1)
 
@@ -37,29 +55,54 @@ def get_toggl(toggl, start_date="2021-09-03", end_date="2021-09-03"):
 
     # Getting names of projects
     all_pid = [int(x) for x in df_toggl.pid.unique() if str(x) != "nan"]
-    dct_cid_pid = {p: toggl.getProject(p)['data'] for p in all_pid}
-    df_cid_pid_dict = {'pid': dct_cid_pid.keys(),
-                       'project': [dct_cid_pid[k]['name'] for k in dct_cid_pid.keys()],
-                       'cid': [dct_cid_pid[k]['cid'] for k in dct_cid_pid.keys()]}
+    dct_cid_pid = {p: toggl.getProject(p)["data"] for p in all_pid}
+    df_cid_pid_dict = {
+        "pid": dct_cid_pid.keys(),
+        "project": [dct_cid_pid[k]["name"] for k in dct_cid_pid.keys()],
+        "cid": [dct_cid_pid[k]["cid"] for k in dct_cid_pid.keys()],
+    }
     df_cid_pid = pd.DataFrame.from_dict(df_cid_pid_dict)
-    df_toggl = pd.merge(df_toggl, df_cid_pid[['pid', 'project', 'cid']], on=['pid'], how='left')
+
+    # Merging project name and client id into df_toggl
+    df_toggl = pd.merge(
+        df_toggl, df_cid_pid[["pid", "project", "cid"]], on=["pid"], how="left"
+    )
 
     # All clients retrieved from toggl
     all_clients = toggl.getClients()
     df_clients = pd.DataFrame.from_dict(all_clients)
-    df_clients.rename({'id': 'cid', 'name': 'client'}, axis=1, inplace=True)
-    df_toggl = pd.merge(df_toggl, df_clients[['cid', 'client']], on=['cid'], how='left')
+    df_clients.rename({"id": "cid", "name": "client"}, axis=1, inplace=True)
+
+    # Merging client column into df_toggl
+    df_toggl = pd.merge(df_toggl, df_clients[["cid", "client"]], on=["cid"], how="left")
 
     df_toggl["h_registradas"] = df_toggl["duration"] / 3600
     df_toggl["lunes_semana"] = df_toggl["date"].apply(
         lambda x: x - timedelta(days=x.weekday() % 7)
     )
-    df_toggl["lunes_semana"] = df_toggl["lunes_semana"].astype(
-        "datetime64[ns]"
-    )
+    df_toggl["lunes_semana"] = df_toggl["lunes_semana"].astype("datetime64[ns]")
 
+    # Getting all workspaces
+    all_workspaces = toggl.getWorkspaces()
+    df_worspaces = pd.DataFrame.from_dict(all_workspaces)
+    df_worspaces.rename({"id": "wid", "name":"workspace"}, axis=1, inplace=True)
+    # Merging workspaces into df_toggl
+    df_toggl = pd.merge(df_toggl, df_worspaces[["wid", "workspace"]], how="left", on="wid")
 
-    df_toggl = df_toggl[['date', 'client', 'project', 'h_registradas', 'description', 'start', 'lunes_semana']]
+    # Merging workspaces into toggl
+
+    df_toggl = df_toggl[
+        [
+            "date",
+            "client",
+            "project",
+            "h_registradas",
+            "description",
+            "start",
+            "lunes_semana",
+            "workspace",
+        ]
+    ]
 
     return df_toggl
 
@@ -73,7 +116,7 @@ def df_toggl_to_json_files(path, df_toggl):
         df_to_json = df_toggl[df_toggl.date == d]
         entries = df_to_json.to_json()
         file_path = "{}/{}.json".format(path, d)
-        with open(file_path, 'w') as f:
+        with open(file_path, "w") as f:
             f.write(entries)
     ok = 1
     return ok
@@ -87,7 +130,7 @@ def read_cache_toggl(path, start_date, end_date):
     for d in dates:
         file_path = "{}/{}.json".format(path, d)
         try:
-            with open(file_path, 'r') as f:
+            with open(file_path, "r") as f:
                 json_string = f.read()
             df = pd.read_json(json_string)
             df_toggl = pd.concat([df, df_toggl])
@@ -97,8 +140,6 @@ def read_cache_toggl(path, start_date, end_date):
     return df_toggl
 
 
-
-
 # """
 # TODO: turn this three lines into a function
 # """
@@ -106,4 +147,3 @@ def read_cache_toggl(path, start_date, end_date):
 # df_toggl = toggl_entries_to_df(toggl_entries)
 
 # df_toggl = read_cache_toggl(path, start_date, end_date)
-
--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -12,21 +12,32 @@ toggl_token = f.read()
 f.close()
 toggl.setAPIKey(toggl_token)
 
-start_date = "2021-09-01"
+start_date = "2021-08-30"
 end_date = "2021-09-06"
 path = "../data/toggl"
 
 df_toggl = tg.get_toggl(toggl, start_date, end_date)
 export_to_json = tg.df_toggl_to_json_files(path, df_toggl)
-df_summary = df_toggl[['lunes_semana', 'client', 'project', 'h_registradas']].groupby(by=['lunes_semana', 'client', 'project'], as_index=False).sum()
+df_summary = (
+    df_toggl[["lunes_semana", "client", "project", "h_registradas"]]
+    .groupby(by=["lunes_semana", "client", "project"], as_index=False)
+    .sum()
+)
+
+df_summary_ws = (
+    df_toggl[["lunes_semana", "client", "project", "h_registradas", "workspace"]]
+    .groupby(by=["lunes_semana", "workspace"], as_index=False)
+    .sum()
+)
+
+print(df_summary_ws.head())
 
 asignacion_path = "../data/asignacion.xlsx"
 df_asig = xl.get_asignacion(asignacion_path)
 
-df_toggl_asig = pd.merge(df_asig, df_summary, how='outer', on=['lunes_semana', 'client', 'project'])
+df_toggl_asig = pd.merge(
+    df_asig, df_summary, how="outer", on=["lunes_semana", "client", "project"]
+)
 
 
-df = io.read_cache_toggl(path, start_date, end_date)
-
-
-
+df_toggl = tg.read_cache_toggl(path, start_date, end_date)
